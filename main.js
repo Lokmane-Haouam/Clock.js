3e5399adbe809e58c1d59257b92fcec833287b9f
Lokmane-Haouam <houamlokman04@gmail.com>
2025-01-20 11:41:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -23,9 +23,9 @@ let days = [
 ];
 function clock() {
   let time = new Date();
-  document.getElementById("h").textContent = String(time.getHours());
-  document.getElementById("m").textContent = String(time.getMinutes());
-  document.getElementById("s").textContent = String(time.getSeconds());
+  document.getElementById("h").textContent = String(time.getHours()).padStart(2, "0");
+  document.getElementById("m").textContent = String(time.getMinutes()).padStart(2, "0");
+  document.getElementById("s").textContent = String(time.getSeconds()).padStart(2, "0");
   document.getElementById("w").textContent = days[time.getDay()];
   document.getElementById("mo").textContent = months[time.getMonth()];
   document.getElementById("d").textContent = String(time.getDate());
